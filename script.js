cf4ec41236b729829955a1b260ab7cf813206f55
Azmat2575 <84296155+Azmat2575@users.noreply.github.com>
2025-03-10 23:14:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,92 @@
-let balance = 5000;
+function getUSSDCode() {
+    let network = document.getElementById("network").value;
+    let ussdType = document.getElementById("ussd-type").value;
+    let packageType = document.getElementById("package-type").value;
+    let ussdCode = "";
+
+    let ussdCodes = {
+        jazz: {
+            balance: "*111#",
+            internet: {
+                daily: "*117*4#",
+                weekly: "*117*47#",
+                monthly: "*117*30#"
+            },
+            call: {
+                daily: "*340#",
+                weekly: "*430#",
+                monthly: "*710#"
+            },
+            sms: {
+                daily: "*101*1*01#",
+                weekly: "*101*1*07#",
+                monthly: "*101*1*30#"
+            }
+        },
+        telenor: {
+            balance: "*444#",
+            internet: {
+                daily: "*345*88#",
+                weekly: "*345*97#",
+                monthly: "*345*59#"
+            },
+            call: {
+                daily: "*345*20#",
+                weekly: "*345*45#",
+                monthly: "*345*60#"
+            },
+            sms: {
+                daily: "*345*1#",
+                weekly: "*345*2#",
+                monthly: "*345*3#"
+            }
+        },
+        zong: {
+            balance: "*222#",
+            internet: {
+                daily: "*6464*1#",
+                weekly: "*6464*2#",
+                monthly: "*6464*3#"
+            },
+            call: {
+                daily: "*456*1#",
+                weekly: "*456*7#",
+                monthly: "*456*30#"
+            },
+            sms: {
+                daily: "*700#",
+                weekly: "*702#",
+                monthly: "*705#"
+            }
+        },
+        ufone: {
+            balance: "*124#",
+            internet: {
+                daily: "*3*1#",
+                weekly: "*3*7#",
+                monthly: "*3*30#"
+            },
+            call: {
+                daily: "*888#",
+                weekly: "*1000#",
+                monthly: "*707#"
+            },
+            sms: {
+                daily: "*105*1#",
+                weekly: "*105*7#",
+                monthly: "*105*30#"
+            }
+        }
+    };
+
+    if (ussdType === "balance") {
+        ussdCode = ussdCodes[network].balance;
+    } else {
+        ussdCode = ussdCodes[network][ussdType][packageType];
+    }
+
+    document.getElementById("ussd-result").innerText = ussdCode ? `Dial: ${ussdCode}` : "USSD Code not found!";
+}let balance = 5000;
 let history = [];
 
 function updateHistory(transaction) {
